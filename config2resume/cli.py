a16a4bd5205b595ea67d44e54763df09a0b737a9
Prateek Tripathi <ptripathi@mt.iitr.ac.in>
2024-05-22 01:31:32 +0530

    fix template, config and cli

--- config2resume/cli.py
+++ config2resume/cli.py
@@ -3,13 +3,13 @@ from util.parser import parse_config
 from util.resume_generator import generate
 
 @click.command()
-@click.option('--config', required=True, help='Path to config file')
-@click.option('--template', default='templates/sample_template.yaml', help='Path to config file')
-@click.option('--output', default='./resume.pdf', help='Path where resume needs to be stored')
+@click.option('--config', default='./config/sample_config.yaml', help='Path to config file')
+@click.option('--template', default='./templates/sample_template.html', help='Path to config file')
+@click.option('--output', default='./output/resume.pdf', help='Path where resume needs to be stored')
 def generate_resume(config, template, output):
     """CLI command to generate resume"""
     config_data = parse_config(config)
-    generate(config, template, output)
+    generate(config_data, template, output)
     click.echo(f"Resume generated successfully at {output}")
 
 if __name__ == "__main__":
