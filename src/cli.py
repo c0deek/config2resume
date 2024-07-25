edac221944c8116bf7a13648a3cf6b15d4be2c88
Prateek Tripathi <ptripathi@mt.iitr.ac.in>
2024-07-26 00:30:50 +0530

    updated ats resume template

--- src/cli.py
+++ src/cli.py
@@ -3,8 +3,8 @@ from util.parser import parse_config
 from util.resume_generator import generate
 
 @click.command()
-@click.option('--config', default='./config/sample_config.yaml', help='Path to config file')
-@click.option('--template', default='./templates/sample_template.html', help='Path to config file')
+@click.option('--config', default='./config/sample_config_ats.yaml', help='Path to config file')
+@click.option('--template', default='./templates/sample_template_ats.html', help='Path to config file')
 @click.option('--output', default='./output/resume.pdf', help='Path where resume needs to be stored')
 def generate_resume(config, template, output):
     """CLI command to generate resume"""
